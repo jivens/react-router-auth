4335ebfaeb453039772603ed8bbb889dc4a77a1e
John Ivens <john.wagner.ivens@gmail.com>
2020-03-13 15:54:33 -0700

    Role based permissions on private routes are working

---
+++ src/access/permissions.js
@@ -0,0 +1,11 @@
+export const path_role_permissions = {
+    '/admin': ['admin'],
+    '/home' : ['*'],
+}
+
+export const path_role_field_permissions = {
+    '/spelling': {
+        'all_fields': ['root', 'reichard', 'english', 'doak'],
+        'view': ['root', 'reichard', 'english']
+    }
+}
