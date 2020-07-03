5f2ae74fc5ee61fa50899494ed2dfb839a2d1c38
amyfou <a.v.fountain@gmail.com>
2020-07-03 15:55:10 -0700

    styled affix table and included all columns, re-enstated derived rows, added selectFilter which is not yet server-side filtering

--- src/pages/AffixTable.js
+++ src/pages/AffixTable.js
@@ -1,40 +1,10 @@
 import React from 'react'
-import styled from 'styled-components'
 import { useTable, usePagination, useSortBy, useFilters, useGlobalFilter  } from 'react-table'
 import { DefaultColumnFilter, GlobalFilter, fuzzyTextFilterFn, SelectColumnFilter } from '../utils/Filters'
 import { useAuth } from "../context/auth";
-import { useQuery } from '@apollo/react-hooks'
 import { getAffixesQuery } from './../queries/queries'
 import { sortReshape, filterReshape } from "./../utils/reshapers"
-
-const Styles = styled.div`
-  padding: 1rem;
-  table {
-    border-spacing: 0;
-    border: 1px solid black;
-    tr {
-      :last-child {
-        td {
-          border-bottom: 0;
-        }
-      }
-    }
-    th,
-    td {
-      margin: 0;
-      padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
-      :last-child {
-        border-right: 0;
-      }
-    }
-  }
-  .pagination {
-    padding: 0.5rem;
-  }
-`
-
+import TableStyles from "./../stylesheets/table-styles"
 
 function Table({
   columns,
@@ -122,7 +92,7 @@ function Table({
   // Render the UI for your table
   return (
     <>
-      <pre>
+      {/* <pre>
         <code>
           {JSON.stringify(
             {
@@ -139,9 +109,20 @@ function Table({
             2
           )}
         </code>
-      </pre>
+      </pre> */}
       <table {...getTableProps()}>
         <thead>
+          <tr>
+            <th
+              colSpan={visibleColumns.length}
+            >
+              <GlobalFilter
+                preGlobalFilteredRows={preGlobalFilteredRows}
+                globalFilter={state.globalFilter}
+                setGlobalFilter={setGlobalFilter}
+              />
+            </th>
+          </tr>
           {headerGroups.map(headerGroup => (
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map(column => (
@@ -161,20 +142,6 @@ function Table({
               ))}
             </tr>
           ))}
-          <tr>
-            <th
-              colSpan={visibleColumns.length}
-              style={{
-                textAlign: 'left',
-              }}
-            >
-              <GlobalFilter
-                preGlobalFilteredRows={preGlobalFilteredRows}
-                globalFilter={state.globalFilter}
-                setGlobalFilter={setGlobalFilter}
-              />
-            </th>
-          </tr>
         </thead>
         <tbody {...getTableBodyProps()}>
           {page.map((row, i) => {
@@ -254,16 +221,36 @@ function AffixTable() {
   const columns = React.useMemo(
     () => [
       {
-        Header: 'ID',
-        accessor: 'id',
-        tableName: 'AffixTable',
+        Header: 'Edit/Delete',
+        disableFilters: true,
+        sortable: false,
+        width: 100,
         show: false,
-        id: 'id'
-      },
+        id: 'editDelete',
+        tableName: 'AffixTable',
+        Cell: ({row, original}) => (
+          <div>
+            <button>
+                !E
+            </button>
+            <button>
+                !X
+            </button>
+          </div>
+        )
+      }, 
+      // {
+      //   Header: 'ID',
+      //   accessor: 'id',
+      //   tableName: 'AffixTable',
+      //   show: false,
+      //   disableFilters: true,
+      //   id: 'id'
+      // },
       {
         Header: 'Type',
         accessor: 'type',
-        //Filter: SelectColumnFilter,
+        Filter: SelectColumnFilter,
         tableName: 'AffixTable',
         show: true,
         id: 'type'
@@ -271,33 +258,37 @@ function AffixTable() {
       {
         Header: 'Nicodemus',
         accessor: 'nicodemus',
-        //filter: 'fuzzyText',
         tableName: 'AffixTable',
         show: true,
         id: 'nicodemus'
       },
       {
+        Header: 'Salish',
+        accessor: 'salish',
+        filter: 'fuzzyText',
+        tableName: 'AffixTable',
+        show: true,
+        id: 'salish'
+      },
+      {
         Header: 'English',
         accessor: 'english',
-        // filter: 'fuzzyText',
         tableName: 'AffixTable',
         show: true,
         id: 'english'
       },
       {
         Header: 'Link',
-        accessor: 'link',
-        disableFilters: true,
-        //Cell: ({ row }) => <a href={row.original.link} target="_blank" rel="noopener noreferrer">{row.original.page}</a>,
+        accessor: 'page',
+        Cell: ({ row }) => <a href={row.original.link} target="_blank" rel="noopener noreferrer">{row.original.page}</a>,
         tableName: 'AffixTable',
         show: true,
-        id: 'link'
+        id: 'page'
       },
       {
         Header: 'Username',
         accessor: 'user.username',
-        //Filter: SelectColumnFilter,
-        //filter: 'includes',
+        Filter: SelectColumnFilter,
         tableName: 'AffixTable',
         show: false,
         id: 'username'
@@ -305,30 +296,30 @@ function AffixTable() {
       {
         Header: 'Active',
         accessor: 'active',
-        //filter: 'fuzzyText',
+        Filter: SelectColumnFilter,
+        width: 50,
         tableName: 'AffixTable',
         show: false,
         id: 'active'
       },
       {
-        Header: 'Edit/Delete',
-        filterable: false,
-        sortable: false,
-        width: 100,
-        show: false,
-        id: 'editDelete',
+        Header: 'Prev. ID',
+        accessor: 'prevId',
+        width: 50,
+        disableFilters: true,
         tableName: 'AffixTable',
-        // Cell: ({row, original}) => (
-        //   <div>
-        //     <button>
-        //         !E
-        //     </button>
-        //     <button>
-        //         !X
-        //     </button>
-        //   </div>
-        // )
-      }, 
+        show: false,
+        id: 'prevId'
+      },
+      {
+        Header: 'Edit Note',
+        accessor: 'editnote',
+        tableName: 'AffixTable',
+        disableFilters: true,
+        sortable: false,
+        show: false,
+        id: 'editnote'
+      },
     ], []
   )
 
@@ -350,14 +341,6 @@ function AffixTable() {
        }
     })
     return data
-    // .then((affixData) => {
-    //   console.log(affixData.data.affixes)
-    //   return affixData.data.affixes
-    // })
-    // .catch((error) => {
-    //   console.log(error)
-    //   return []
-    // })  
   }  
 
   const fetchData = React.useCallback(({ pageSize, pageIndex, sortBy, filters, globalFilter }) => {
@@ -376,7 +359,7 @@ function AffixTable() {
       // Only update the data if this is the latest fetch
       if (fetchId === fetchIdRef.current) {
         const controlledSort = sortReshape(sortBy) 
-        const controlledFilter = filterReshape(filters, globalFilter, ["english", "nicodemus"])
+        const controlledFilter = filterReshape(filters, globalFilter, ["english", "nicodemus", "salish", "page"])
         console.log(controlledFilter)
         // reset to first page when filters change
         // if (filters.length > 0) {
@@ -400,7 +383,7 @@ function AffixTable() {
   }, [])
 
   return (
-    <Styles>
+    <TableStyles>
       <Table
         columns={columns}
         data={data}
@@ -408,334 +391,8 @@ function AffixTable() {
         loading={loading}
         pageCount={pageCount}
       />
-    </Styles>
+    </TableStyles>
   )
 }
 
 export default AffixTable
-
-//import React from 'react'
-//import { useTable, usePagination, useSortBy, useFilters, useGlobalFilter, useFlexLayout, useResizeColumns } from 'react-table'
-//import { Button, Segment } from 'semantic-ui-react'
-//import TableStyles from './../stylesheets/table-styles'
-//import { DefaultColumnFilter, GlobalFilter, fuzzyTextFilterFn, SelectColumnFilter } from './../utils/Filters'
-//import { IndeterminateCheckbox } from './../utils/Checkbox'
-
-
-// const headerProps = (props, { column }) => getStyles(props, column.align)
-// const cellProps = (props, { cell }) => getStyles(props, cell.column.align)
-// const getStyles = (props, align = 'left') => [
-//   props,
-//   {
-//     style: {
-//       justifyContent: align === 'right' ? 'flex-end' : 'flex-start',
-//       alignItems: 'flex-start',
-//       display: 'flex',
-//     },
-//   },
-// ]
-
-// function Table({
-//   columns,
-//   data,
-//   setLimit,
-//   setOffset,
-//   loading,
-//    })
-//   { 
-//     console.log(setOffset)
-//     console.log(data)
-//     const filterTypes = React.useMemo(
-//     () => ({
-//       fuzzyText: fuzzyTextFilterFn,
-//       text: (rows, id, filterValue) => {
-//         return rows.filter(row => {
-//           const rowValue = row.values[id]
-//           return rowValue !== undefined
-//             ? String(rowValue)
-//                 .toLowerCase()
-//                 .startsWith(String(filterValue).toLowerCase())
-//             : true
-//         })
-//       },
-//     }),
-//     []
-//   )
-
-//   const defaultColumn = React.useMemo(
-//     () => ({
-//       Filter: DefaultColumnFilter,       // Let's set up our default Filter UI
-//       minWidth: 25, // minWidth is only used as a limit for resizing
-//       width: 50, // width is used for both the flex-basis and flex-grow
-//       maxWidth: 500, // maxWidth is only used as a limit for resizing
-//     }),
-//     []
-//   )
-//   const {
-//     getTableProps,
-//     getTableBodyProps,
-//     headerGroups,
-//     prepareRow,
-//     rows,
-//     page,
-//     state,
-//     flatColumns,
-//     preGlobalFilteredRows,
-//     setGlobalFilter,
-//     canPreviousPage,
-//     canNextPage,
-//     pageOptions,
-//     pageCount,
-//     gotoPage,
-//     nextPage,
-//     previousPage,
-//     setPageSize,
-//     getToggleHideAllColumnsProps,
-//     setHiddenColumns,
-//     state: { pageIndex, pageSize },
-//   } = useTable(
-//     {
-//       columns,
-//       data,
-//       defaultColumn,
-//       filterTypes,
-//       hiddenColumns: columns.filter(column => !column.show).map(column => column.id),
-//     },
-//     useResizeColumns,
-//     useFlexLayout,
-//     useGlobalFilter,
-//     useFilters,
-//     useSortBy,
-//     usePagination
-//   )
-
-// // React.useEffect(() => {
-// // const hiddenColumns = flatColumns.filter((column: any) => !column.show).map((column: any)=> column.id);
-// // setHiddenColumns(hiddenColumns); }, []);
-
-// {/* <li>
-//   <IndeterminateCheckbox {...getToggleHideAllColumnsProps()}/>
-//       Toggle All
-// </li>
-// {flatColumns.map(column => (
-//   <div>
-//     <li key={column.id}>
-//     <label>
-//       <input type="checkbox" {...column.getToggleHiddenProps()}/>{' '}
-//       {column.Header}
-//     </label>
-//     </li>
-//   </div>
-// ))}  */}
-
-
-//   // Render the UI for your table
-//   return (
-//     <React.Fragment>
-//       <div className="columnToggle">
-//         <ul>
-//           <li>
-//             <span>Show/Hide Columns:</span>
-//           </li>
-//         </ul>
-//       </div>
-
-//       <Segment>
-//         <GlobalFilter
-//           preGlobalFilteredRows={preGlobalFilteredRows}
-//           globalFilter={state.globalFilter}
-//           setGlobalFilter={setGlobalFilter}
-//           />
-//       </Segment>
-
-//       <table {...getTableProps()}>
-//         <thead>
-//             {headerGroups.map(headerGroup => (
-//               <tr {...headerGroup.getHeaderGroupProps()} >
-//                 {headerGroup.headers.map(column => (
-//                   <th {...column.getHeaderProps(column.getSortByToggleProps(), headerProps)}>{column.render('Header')}
-//                     {column.canResize && (
-//                       <div
-//                         {...column.getResizerProps()}
-//                         className={`resizer ${
-//                           column.isResizing ? 'isResizing' : ''
-//                         }`}
-//                       />
-//                     )}
-//                     {column.isSorted
-//                       ? column.isSortedDesc
-//                         ? ' 🔽'
-//                         : ' 🔼'
-//                       : ''}
-//                       <div>
-//                       {column.canFilter ? column.render('Filter') : null}
-//                       </div>
-//                   </th>
-//                 ))}
-//               </tr>
-//             ))}
-//           </thead>
-//         <tbody {...getTableBodyProps()}>
-//           {page.map((row, i) => {
-//             prepareRow(row);
-//             return (
-//               <tr {...row.getRowProps()}>
-//                 {row.cells.map(cell => {
-//                   return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-//                 })}
-//               </tr>
-//             )
-//           })}
-//           <tr>
-//             {loading ? (
-//               // Use our custom loading state to show a loading indicator
-//               <td colSpan="10">Loading...</td>
-//             ) : (
-//               <td colSpan="10">
-//                 Showing {page.length} of ~{pageCount * pageSize}{' '}
-//                 results
-//               </td>
-//             )}
-//           </tr>
-//         </tbody>
-//       </table>
-
-//       <div className="pagination">
-//         <button onClick={() => setOffset(0)} disabled={!canPreviousPage}>
-//           {'<<'}
-//         </button>{' '}
-//         <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-//           {'<'}
-//         </button>{' '}
-//         <button onClick={() => nextPage()} disabled={!canNextPage}>
-//           {'>'}
-//         </button>{' '}
-//         <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-//           {'>>'}
-//         </button>{' '}
-//         <span>
-//           Page{' '}
-//           <strong>
-//             {pageIndex + 1} of {pageOptions.length}
-//           </strong>{' '}
-//         </span>
-//         <span>
-//           | Go to page:{' '}
-//           <input
-//             type="number"
-//             defaultValue={pageIndex + 1}
-//             onChange={e => {
-//               const page = e.target.value ? Number(e.target.value) - 1 : 0
-//               gotoPage(page)
-//             }}
-//             style={{ width: '100px' }}
-//           />
-//         </span>{' '}
-//         <select
-//           value={pageSize}
-//           onChange={e => {
-//             setPageSize(Number(e.target.value))
-//           }}
-//         >
-//           {[10, 20, 30, 40, 50].map(pageSize => (
-//             <option key={pageSize} value={pageSize}>
-//               Show {pageSize}
-//             </option>
-//           ))}
-//         </select>
-//       </div>
-//     </React.Fragment>
-//   )
-// }
-
-
-// function AffixTable({setLimit, setOffset, affixes}) {
-//   const columns = React.useMemo(
-//     () => [
-//       {
-//         Header: 'ID',
-//         accessor: 'id',
-//         tableName: 'AffixTable',
-//         show: false,
-//       },
-//       {
-//         Header: 'Type',
-//         accessor: 'type',
-//         Filter: SelectColumnFilter,
-//         tableName: 'AffixTable',
-//         show: true,
-//       },
-//       {
-//         Header: 'Nicodemus',
-//         accessor: 'nicodemus',
-//         filter: 'fuzzyText',
-//         tableName: 'AffixTable',
-//         show: true,
-//       },
-//       {
-//         Header: 'English',
-//         accessor: 'english',
-//         filter: 'fuzzyText',
-//         tableName: 'AffixTable',
-//         show: true,
-//       },
-//       {
-//         Header: 'Link',
-//         accessor: 'link',
-//         disableFilters: true,
-//         Cell: ({ row }) => <a href={row.original.link} target="_blank" rel="noopener noreferrer">{row.original.page}</a>,
-//         tableName: 'AffixTable',
-//         show: true,
-//       },
-//       {
-//         Header: 'Username',
-//         accessor: 'user.username',
-//         Filter: SelectColumnFilter,
-//         filter: 'includes',
-//         tableName: 'AffixTable',
-//         show: false,
-//       },
-//       {
-//         Header: 'Active',
-//         accessor: 'active',
-//         filter: 'fuzzyText',
-//         tableName: 'AffixTable',
-//         show: false,
-//       },
-//       {
-//         Header: 'Edit/Delete',
-//         filterable: false,
-//         sortable: false,
-//         width: 100,
-//         show: false,
-//         tableName: 'AffixTable',
-//         Cell: ({row, original}) => (
-//           <div>
-//             <Button>
-//                 !E
-//             </Button>
-//             <Button>
-//                 !X
-//             </Button>
-//           </div>
-//         )
-//       },
-//     ]
-//   )
-
-//   const [data] = React.useState(() => affixes)
-//   console.log(data)
-
-//   return (
-//     <TableStyles>
-//       <Table
-//         columns={columns}
-//         data={data}
-//         setLimit={setLimit}
-//         setOffset={setOffset}
-//       />
-//     </TableStyles>
-//   )
-// }
-
-// export default AffixTable
