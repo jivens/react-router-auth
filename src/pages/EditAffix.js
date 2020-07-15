43f15d0ca12c24b35d620cbc8242f79fdbad2e2e
amyfou <a.v.fountain@gmail.com>
2020-07-14 18:06:02 -0700

    edit form is styled and includes a cancel button

--- src/pages/EditAffix.js
+++ src/pages/EditAffix.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Redirect, useLocation } from 'react-router-dom';
+import { Redirect, useLocation, useHistory } from 'react-router-dom';
 import { getAffixByIdQuery, updateAffixMutation, getAffixTypesQuery } from './../queries/queries'
-import { Button, Input } from 'semantic-ui-react';
+import { Button, Input, Dropdown, Label, Grid, Header, Message } from 'semantic-ui-react';
 import * as Yup from 'yup';
 import { Formik, Form } from 'formik';
 import { useAuth } from "../context/auth";
@@ -17,12 +17,13 @@ let updateAffixSchema = Yup.object().shape({
   });
 
 
-function EditAffix(props) {
+function EditAffix() {
   const { client } = useAuth();
   const [ hasUpdated, setHasUpdated] = useState(false)
   const search = new URLSearchParams(useLocation().search)
   //console.log(search.get("id"))
   const id = search.get("id")
+  const history = useHistory()
 
 
   let { loading: affixLoading, error: affixError, data: affixData } = useQuery(getAffixByIdQuery, {client: client, variables: {id: id} }) 
@@ -68,17 +69,46 @@ function EditAffix(props) {
     return <Redirect to="/affixes" />;
   }
 
+  function dropDownOptions(options) {
+      let res = []
+      options.map((item) => {
+          let h = {}
+          h = { 
+              key: item.id.toString(),
+              value: item.value,
+              text: item.value          
+          }
+          res.push(h)
+      })
+      return res
+  }
+
+  const routeChange=()=> {
+    let path = `/affixes`;
+    history.push(path);
+  }
+
   return (
+    <>
+    <Grid centered columns>
+        <Grid.Row>
+            <Grid.Column textAlign="center" width={12}>
+                <Header as="h2">Edit an Affix</Header>
+                <Message>The elements whose labels are solid blue are required for all affixes.  The elements whose labels are outlined may be blank.</Message>
+            </Grid.Column>
+        </Grid.Row>
+    </Grid>
     <Formik 
         initialValues={{ 
         id: affixData.affixes_by_pk.id,
         type: affixData.affixes_by_pk.affix_type.id,
+        typeText: affixData.affixes_by_pk.affix_type.value,
         nicodemus: affixData.affixes_by_pk.nicodemus,
-        salish: affixData.affixes_by_pk.salish,
+        salish: affixData.affixes_by_pk.salish ? affixData.affixes_by_pk.salish : "" ,
         english: affixData.affixes_by_pk.english, 
-        link: affixData.affixes_by_pk.link,
-        page: affixData.affixes_by_pk.page,
-        editnote: affixData.affixes_by_pk.editnote
+        link: affixData.affixes_by_pk.link ? affixData.affixes_by_pk.link : "" ,
+        page: affixData.affixes_by_pk.page ? affixData.affixes_by_pk.page : "" ,
+        editnote: affixData.affixes_by_pk.editnote ? affixData.affixes_by_pk.editnote : "" 
         }}
         validationSchema={updateAffixSchema}
         onSubmit={(values, { setSubmitting }) => {
@@ -87,125 +117,153 @@ function EditAffix(props) {
         >
         {({ isSubmitting, values, errors, touched, handleChange, handleBlur }) => (
         <Form>
-            <select
-                name="type"
-                value={values.type}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                style={{ display: 'block' }}
-            >
-                <option value="" label="Select a type" />
-                { typeData.affix_types.map((item) => {
-                    return <option value={item.id.toString()} label={item.value} key={item.id.toString()}/>
-                })}
-            </select>
-            {errors.type &&
-                touched.type &&
-                <div className="input-feedback">
-                {errors.type}
-                </div>}
-            <Input
-                fluid
-                style={{ paddingBottom: '5px' }}
-                icon="write"
-                iconPosition="left"
-                id="nicodemus"
-                placeholder="Nicodemus"
-                type="text"
-                value={ values.nicodemus }
-                onChange={ handleChange }
-                onBlur={ handleBlur }
-                className={ errors.nicodemus && touched.nicodemus ? 'text-input error' : 'text-input' }
-            />
-            {errors.nicodemus && touched.nicodemus && ( <div className="input-feedback">{errors.nicodemus}</div>
-            )}
-            <Input
-                fluid
-                style={{ paddingBottom: '5px' }}
-                icon="write"
-                iconPosition="left"
-                id="salish"
-                placeholder="Salish"
-                type="text"
-                value={ values.salish }
-                onChange={ handleChange }
-                onBlur={ handleBlur }
-                className={ errors.salish && touched.salish ? 'text-input error' : 'text-input' }
-            />
-            {errors.salish && touched.salish && ( <div className="input-feedback">{errors.salish}</div>
-            )}
-            <Input
-                fluid
-                style={{ paddingBottom: '5px' }}
-                icon="write"
-                iconPosition="left"
-                id="english"
-                placeholder="english"
-                type="text"
-                value={ values.english }
-                onChange={ handleChange }
-                onBlur={ handleBlur }
-                className={ errors.english && touched.english ? 'text-input error' : 'text-input'}
-            />
-            {errors.english && touched.english && ( <div className="input-feedback">{errors.english}</div>
-            )}
-            <Input
-                fluid
-                style={{ paddingBottom: '5px' }}
-                icon='write'
-                iconPosition='left'
-                id="link"
-                placeholder="URL"
-                type="text"
-                value={ values.link }
-                onChange={ handleChange }
-                onBlur={ handleBlur }
-                className={ errors.link && touched.link ? 'text-input error' : 'text-input' }
-            />
-            {errors.link && touched.link && ( <div className="input-feedback">{errors.link}</div>
-            )}
-            <Input
-                fluid
-                style={{ paddingBottom: '5px' }}
-                icon='write'
-                iconPosition='left'
-                id="page"
-                placeholder="Link text"
-                type="text"
-                value={ values.page }
-                onChange={ handleChange }
-                onBlur={ handleBlur }
-                className={ errors.page && touched.page ? 'text-input error' : 'text-input' }
-            />
-            {errors.page && touched.page && ( <div className="input-feedback">{errors.page}</div>
-            )}
-            <Input
-                fluid
-                style={{ paddingBottom: '5px' }}
-                icon='write'
-                iconPosition='left'
-                id="editnote"
-                placeholder="Edited to..."
-                type="text"
-                value={ values.editnote }
-                onChange={ handleChange }
-                onBlur={ handleBlur }
-                className={ errors.editnote && touched.editnote ? 'text-input error' : 'text-input' }
-            />
-            {errors.editnote && touched.editnote && ( <div className="input-feedback">{errors.editnote}</div>
-            )}
-            <Button 
-                fluid
-                color="blue" 
-                size="large" 
-                type="submit" 
-                disabled={isSubmitting}
-                >
-                Submit
-                </Button>
-            </Form>
-            )}
-        </Formik>
+            <Grid centered columns>
+                <Grid.Row>
+                    <Grid.Column width={2} textAlign="right"><Label pointing="right" color="blue">Affix Type</Label></Grid.Column>
+                    <Grid.Column width={10}>
+                    <Dropdown
+                        placeholder='Select a Type'
+                        fluid
+                        selection
+                        options = {dropDownOptions(typeData.affix_types)}
+                        value = {values.type.toString()}
+                        text = {values.typeText}
+                    />
+                    {errors.type && touched.type && <div className="input-feedback"> {errors.type} </div>}
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width={2} textAlign="right"><Label pointing="right" color="blue">Nicodemus</Label></Grid.Column>
+                    <Grid.Column width={10}>
+                        <Input
+                            fluid
+                            style={{ paddingBottom: '5px' }}
+                            id="nicodemus"
+                            placeholder="Nicodemus"
+                            type="text"
+                            value={ values.nicodemus }
+                            onChange={ handleChange }
+                            onBlur={ handleBlur }
+                            className={ errors.nicodemus && touched.nicodemus ? 'text-input error' : 'text-input' }
+                        />
+                        {errors.nicodemus && touched.nicodemus && ( <div className="input-feedback">{errors.nicodemus}</div>
+                        )}
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width={2} textAlign="right"><Label pointing="right" basic color="blue">Salish</Label></Grid.Column>
+                    <Grid.Column width={10}>
+                        <Input
+                            fluid
+                            style={{ paddingBottom: '5px' }}
+                            id="salish"
+                            placeholder="Salish"
+                            type="text"
+                            value={ values.salish }
+                            onChange={ handleChange }
+                            onBlur={ handleBlur }
+                            className={ errors.salish && touched.salish ? 'text-input error' : 'text-input' }
+                        />
+                        {errors.salish && touched.salish && ( <div className="input-feedback">{errors.salish}</div>
+                        )}
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width={2} textAlign="right"><Label pointing="right" color="blue">English</Label></Grid.Column>
+                    <Grid.Column width={10}>
+                        <Input
+                            fluid
+                            style={{ paddingBottom: '5px' }}
+                            id="english"
+                            placeholder="english"
+                            type="text"
+                            value={ values.english }
+                            onChange={ handleChange }
+                            onBlur={ handleBlur }
+                            className={ errors.english && touched.english ? 'text-input error' : 'text-input'}
+                        />
+                        {errors.english && touched.english && ( <div className="input-feedback">{errors.english}</div>
+                        )}
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width={2} textAlign="right"><Label basic pointing="right" color="blue">Link</Label></Grid.Column>
+                    <Grid.Column width={10}>
+                        <Input
+                            fluid
+                            style={{ paddingBottom: '5px' }}
+                            id="link"
+                            placeholder="URL"
+                            type="text"
+                            value={ values.link }
+                            onChange={ handleChange }
+                            onBlur={ handleBlur }
+                            className={ errors.link && touched.link ? 'text-input error' : 'text-input' }
+                        />
+                        {errors.link && touched.link && ( <div className="input-feedback">{errors.link}</div>
+                        )}
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width={2} textAlign="right"><Label basic pointing="right" color="blue">Link Text</Label></Grid.Column>
+                    <Grid.Column width={10}>
+                        <Input
+                            fluid
+                            style={{ paddingBottom: '5px' }}
+                            id="page"
+                            placeholder="Link text"
+                            type="text"
+                            value={ values.page }
+                            onChange={ handleChange }
+                            onBlur={ handleBlur }
+                            className={ errors.page && touched.page ? 'text-input error' : 'text-input' }
+                        />
+                        {errors.page && touched.page && ( <div className="input-feedback">{errors.page}</div>
+                        )}
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width={2} textAlign="right"><Label pointing="right" color="blue">Note</Label></Grid.Column>
+                    <Grid.Column width={10}>
+                        <Input
+                            fluid
+                            style={{ paddingBottom: '5px' }}
+                            id="editnote"
+                            placeholder="An edit note is required"
+                            type="text"
+                            value={ values.editnote }
+                            onChange={ handleChange }
+                            onBlur={ handleBlur }
+                            className={ errors.editnote && touched.editnote ? 'text-input error' : 'text-input' }
+                        />
+                        {errors.editnote && touched.editnote && ( <div className="input-feedback">{errors.editnote}</div>
+                        )}
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width={4}>
+                        <Button 
+                            fluid
+                            color="blue"  
+                            type="submit" 
+                            disabled={isSubmitting}
+                        >
+                            Submit
+                        </Button>
+                    </Grid.Column>
+                    <Grid.Column width={4}>
+                        <Button onClick={routeChange}
+                            fluid
+                        >
+                            Cancel
+                        </Button>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </Form>
+         )}
+    </Formik>
+    </>
   );
 }
 
